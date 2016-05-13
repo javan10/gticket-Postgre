914c9dfedf75ba9926a422d0f8d7516233f91ca2
javan <javan1000@gmail.com>
2016-05-13 19:38:47 -0300

    bd ok gemerated id ok

--- grails-app/domain/gticket/Estabelecimento.groovy
+++ grails-app/domain/gticket/Estabelecimento.groovy
@@ -1,11 +1,17 @@
 package gticket
 
 class Estabelecimento {
+
 	String nome
 	String endereco
 	Boolean temEstacionamento
 	
+	static mapping = {
+		id generator: 'identity'
+
+	}
     static constraints = {
+		
 		nome nullable:false, blank:false, maxSize:128, unique:true
 		endereco nullable:false, blank:false, maxSize:256
 		temEstacionamento nullable:false, blank:false
